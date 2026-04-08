16a06e59b9bb93d618748e47fb3f7dbc303f948c
Gabriel de Quadros Ligneul <gligneul@offchainlabs.com>
2026-04-08 10:00:26 -0300

    Increase concurrency of setup

--- crates/cli/src/util/utils.rs
+++ crates/cli/src/util/utils.rs
@@ -28,7 +28,7 @@ use tokio::sync::Semaphore;
 use tracing::{debug, info, warn};
 
 /// Maximum number of concurrent funding tasks to avoid overwhelming the RPC with connections.
-const FUNDING_CONCURRENCY_LIMIT: usize = 25;
+const FUNDING_CONCURRENCY_LIMIT: usize = 1000;
 
 pub const DEFAULT_PRV_KEYS: [&str; 10] = [
     "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
